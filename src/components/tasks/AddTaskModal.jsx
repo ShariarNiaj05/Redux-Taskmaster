c06f012656b3a9148c52112780dcd20dcd5c8673
Shariar Islam <shariarn85@gmail.com>
2024-02-22 13:07:59 +0600

    half done for the Add Task form

--- src/components/tasks/AddTaskModal.jsx
+++ src/components/tasks/AddTaskModal.jsx
@@ -1,13 +1,35 @@
 import Modal from "../ui/Modal";
+import { useForm } from "react-hook-form";
 
 const AddTaskModal = ({ isOpen, setIsOpen }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div>
-      <Modal isOpen={isOpen} setIsOpen={setIsOpen} title={"Add Task To The List"}>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
-          dolorum!{" "}
-        </p>
+      <Modal
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        title={"Add Task To The List"}
+      >
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className=" flex flex-col gap-3">
+            <label htmlFor="title">Title</label>
+
+            <input
+              className=" w-full rounded-md"
+              id="title"
+              type="text"
+              {...register("title")}
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
       </Modal>
     </div>
   );
