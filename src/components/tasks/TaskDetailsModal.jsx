b49210468f5b392a065f776195074376fa86e6cf
Shariar Islam <shariarn85@gmail.com>
2024-03-13 07:02:20 +0600

    handling Task Details Modal !wq

---
+++ src/components/tasks/TaskDetailsModal.jsx
@@ -0,0 +1,12 @@
+import { useSelector } from "react-redux";
+import Modal from "../ui/Modal";
+
+const TaskDetailsModal = ({ isOpen, setIsOpen, id }) => {
+  const { tasks } = useSelector((state) => state.tasksSlice);
+  const task = tasks.find((item) => item.id === id);
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen} title={task?.title}></Modal>
+  );
+};
+
+export default TaskDetailsModal;
